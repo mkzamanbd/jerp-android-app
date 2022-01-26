eaec08aa2528e21851845ef28cc3fe4efc1990d8
Kamruzzaman <zaman7u@gmail.com>
2022-01-27 00:25:06 +0600

    add bottom navigation bar

--- app/src/main/java/com/example/simplelogin/ui/view/DashboardActivity.kt
+++ app/src/main/java/com/example/simplelogin/ui/view/DashboardActivity.kt
@@ -1,17 +1,23 @@
 package com.example.simplelogin.ui.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.simplelogin.R
 import com.example.simplelogin.data.UserPreferences
 import com.example.simplelogin.ui.view.auth.AuthActivity
 import com.example.simplelogin.ui.viewModel.ProfileViewModel
 import com.example.simplelogin.utils.startNewActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+
 
 @AndroidEntryPoint
 class DashboardActivity : AppCompatActivity() {
@@ -22,6 +28,22 @@ class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.setupWithNavController(navController)
+
+//        val appBarConfiguration = AppBarConfiguration.Builder(
+//            R.id.userFragment,
+//            R.id.profileFragment,
+//            R.id.notificationFragment,
+//            R.id.settingsFragment
+//        ).build()
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+//        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 
     fun performLogout() = lifecycleScope.launch {
