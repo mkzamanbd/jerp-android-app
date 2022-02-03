12513d312e9265dcc9f2451cb7ad5803da6f87eb
Kamruzzaman <zaman7u@gmail.com>
2022-02-04 01:05:19 +0600

    Update UserDetailFragment.kt

--- app/src/main/java/com/example/mvvm/ui/view/UserDetailFragment.kt
+++ app/src/main/java/com/example/mvvm/ui/view/UserDetailFragment.kt
@@ -3,7 +3,6 @@ package com.example.mvvm.ui.view
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -28,11 +27,10 @@ class UserDetailFragment : BaseFragment<FragmentUserDetailBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         userId = arguments?.getString("userId")
-        if (userId != null) {
-            Toast.makeText(requireContext(), userId, Toast.LENGTH_SHORT).show()
-        }
 
-        getUserDetail(userId.toString())
+        if (userId != null) {
+            getUserDetail(userId.toString())
+        }
 
         viewModel.userDetail.observe(viewLifecycleOwner, Observer {
             when(it){
