3f7542184655936e3bd9d1639cf65a94d868cde4
Kamruzzaman <zaman7u@gmail.com>
2022-02-09 00:14:15 +0600

    add bottom navigation menu dynamically

--- app/src/main/java/com/example/mvvm/ui/view/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/mvvm/ui/view/fragments/DashboardFragment.kt
@@ -3,14 +3,32 @@ package com.example.mvvm.ui.view.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.mvvm.R
 import com.example.mvvm.base.BaseActivity
 import com.example.mvvm.base.BaseFragment
+import com.example.mvvm.data.response.UserChildMenuModel
+import com.example.mvvm.data.response.UserParentMenuModel
 import com.example.mvvm.databinding.FragmentDashboardBinding
 import com.example.mvvm.network.Resource
 import com.example.mvvm.ui.viewModel.CommonViewModel
+import com.example.mvvm.utils.Constants.Companion.CUSTOMER_LIST
+import com.example.mvvm.utils.Constants.Companion.DCR
+import com.example.mvvm.utils.Constants.Companion.DELIVERY
+import com.example.mvvm.utils.Constants.Companion.DEPOSIT
+import com.example.mvvm.utils.Constants.Companion.NOTICE
+import com.example.mvvm.utils.Constants.Companion.OFFER
+import com.example.mvvm.utils.Constants.Companion.ORDER
+import com.example.mvvm.utils.Constants.Companion.PAYMENT_COLLECTION
+import com.example.mvvm.utils.Constants.Companion.PRODUCT_LIST
+import com.example.mvvm.utils.Constants.Companion.RETURN
+import com.example.mvvm.utils.Constants.Companion.REVIEW_MTP
+import com.example.mvvm.utils.Constants.Companion.REVIEW_ORDER
+import com.example.mvvm.utils.Constants.Companion.REVIEW_REQUEST
+import com.example.mvvm.utils.Constants.Companion.TA_DA
+import com.example.mvvm.utils.Constants.Companion.TRACKING
 import com.example.mvvm.utils.handleApiError
 import com.example.mvvm.utils.visible
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,7 +50,7 @@ class DashboardFragment : BaseFragment<FragmentDashboardBinding>(
         super.onViewCreated(view, savedInstanceState)
         (activity as BaseActivity).hideToolbar() //display toolbar
 
-        binding.tvProductList.setOnClickListener {
+        binding.bottomCenterMenu.setOnClickListener {
             findNavController().navigate(R.id.action_dashboardFragment_to_productFragment)
         }
 
@@ -42,8 +60,8 @@ class DashboardFragment : BaseFragment<FragmentDashboardBinding>(
             binding.progressBar.visible(it is Resource.Loading)
             when (it) {
                 is Resource.Success -> {
-                    Log.d("mobileMenu", it.value.data.toString())
                     binding.tvMobileMenu.text = it.value.data.toString()
+                    setBottomMenu(it.value.data.bottomParentMenu)
                 }
                 is Resource.Failure -> handleApiError(it) {
                     getMobileMenu()
@@ -55,5 +73,92 @@ class DashboardFragment : BaseFragment<FragmentDashboardBinding>(
 
     private fun getMobileMenu() {
         viewModel.getMobileMenu()
+    }
+
+    private fun setBottomMenu(menuModels: List<UserParentMenuModel>) {
+        val bottomMenu: MutableList<UserChildMenuModel> = ArrayList()
+        for (bottom in menuModels) {
+            if (bottom.menuName == "BOTTOM") {
+                bottomMenu.addAll(bottom.menuItems)
+            }
+        }
+        val fabCenter = binding.bottomCenterMenu
+        val firstMenu = binding.bottomFirstMenu
+        val secondMenu = binding.bottomSecondMenu
+        val thirdMenu = binding.bottomThirdMenu
+        val lastMenu = binding.bottomLastMenu
+
+        val ivFirst = binding.ivBottomFirst
+        val ivSecond = binding.ivBottomSecond
+        val ivThird = binding.ivBottomThird
+        val ivLast = binding.ivBottomLast
+
+        if (bottomMenu.size == 5) {
+            for ((count, bottom) in bottomMenu.withIndex()) {
+                when (count) {
+                    0 -> {
+                        firstMenu.text = bottom.menuName
+                        ivFirst.setImageDrawable(ContextCompat.getDrawable(mContext,
+                            getMenuIcon(bottom.featureId)))
+                    }
+                    1 -> {
+                        secondMenu.text = bottom.menuName
+                        ivSecond.setImageDrawable(ContextCompat.getDrawable(mContext,
+                            getMenuIcon(bottom.featureId)))
+
+                    }
+                    2 -> {
+                        when (bottom.featureId) {
+                            ORDER -> {
+                                fabCenter.setImageDrawable(ContextCompat.getDrawable(mContext,
+                                    R.drawable.ic_baseline_shopping_basket_24))
+                            }
+                            TRACKING -> {
+                                fabCenter.setImageDrawable(ContextCompat.getDrawable(mContext,
+                                    R.drawable.ic_hm_track))
+                            }
+                            DELIVERY -> {
+                                fabCenter.setImageDrawable(ContextCompat.getDrawable(mContext,
+                                    R.drawable.ic_hm_delivery))
+                            }
+                            else -> {
+                                fabCenter.setImageDrawable(ContextCompat.getDrawable(mContext,
+                                    R.drawable.ic_no_image_hm))
+                            }
+                        }
+                    }
+                    3 -> {
+                        thirdMenu.text = bottom.menuName
+                        ivThird.setImageDrawable(ContextCompat.getDrawable(mContext,
+                            getMenuIcon(bottom.featureId)))
+                    }
+                    4 -> {
+                        lastMenu.text = bottom.menuName
+                        ivLast.setImageDrawable(ContextCompat.getDrawable(mContext,
+                            getMenuIcon(bottom.featureId)))
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getMenuIcon(featureId: String): Int {
+        when (featureId) {
+            ORDER -> return R.drawable.ic_bt_order_unselect
+            DEPOSIT -> return R.drawable.ic_bt_deposit_unselect
+            TA_DA -> return R.drawable.ic_bt_ta_da_unselect
+            NOTICE -> return R.drawable.ic_bt_warning_unselect
+            DCR -> return R.drawable.ic_bt_visit_un_select
+            TRACKING -> return R.drawable.ic_bt_tracking_unselect
+            RETURN -> return R.drawable.ic_bt_return
+            OFFER -> return R.drawable.ic_bt_offers
+            PAYMENT_COLLECTION -> return R.drawable.ic_bt_payments
+            PRODUCT_LIST -> return R.drawable.ic_bt_product
+            CUSTOMER_LIST -> return R.drawable.ic_bt_customer
+            REVIEW_ORDER -> return R.drawable.ic_hm_review_order
+            REVIEW_REQUEST -> return R.drawable.ic_hm_review
+            REVIEW_MTP -> return R.drawable.ic_hm_review_mtp
+        }
+        return R.drawable.ic_no_image_hm
     }
 }
