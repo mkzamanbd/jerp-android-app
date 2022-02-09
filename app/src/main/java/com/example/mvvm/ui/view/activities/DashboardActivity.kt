84809e2ff4a6235ff0435313474b0d8076f006d0
Kamruzzaman <zaman7u@gmail.com>
2022-02-09 23:02:02 +0600

    add dashboard view binding

--- app/src/main/java/com/example/mvvm/ui/view/activities/DashboardActivity.kt
+++ app/src/main/java/com/example/mvvm/ui/view/activities/DashboardActivity.kt
@@ -1,24 +1,19 @@
 package com.example.mvvm.ui.view.activities
 
 import android.os.Bundle
-import android.widget.Toast
 import android.util.Log
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.mvvm.R
 import com.example.mvvm.adapter.MenuParentAdapter
 import com.example.mvvm.base.BaseActivity
 import com.example.mvvm.data.response.UserChildMenuModel
 import com.example.mvvm.data.response.UserParentMenuModel
 import com.example.mvvm.database.SharedPreferenceManager
+import com.example.mvvm.databinding.ActivityDashboardBinding
 import com.example.mvvm.network.Resource
 import com.example.mvvm.ui.viewModel.CommonViewModel
 import com.example.mvvm.utils.Constants.Companion.CUSTOMER_LIST
@@ -40,7 +35,6 @@ import com.example.mvvm.utils.handleActivityApiError
 import com.example.mvvm.utils.menuRouting
 import com.example.mvvm.utils.startNewActivity
 import com.example.mvvm.utils.visible
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -52,53 +46,17 @@ class DashboardActivity : BaseActivity() {
     lateinit var prefManager: SharedPreferenceManager
     private val viewModel by viewModels<CommonViewModel>()
     private lateinit var homeMenuParentAdapter: MenuParentAdapter
-
-    private lateinit var homeRecyclerView: RecyclerView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var bottomMenuRoot: CoordinatorLayout
-    private lateinit var lnFirst: LinearLayout
-    private lateinit var lnSecond: LinearLayout
-    private lateinit var lnThird: LinearLayout
-    private lateinit var lnLast: LinearLayout
-
-    private lateinit var bottomCenterMenu: FloatingActionButton
-    private lateinit var firstMenu: TextView
-    private lateinit var secondMenu: TextView
-    private lateinit var thirdMenu: TextView
-    private lateinit var lastMenu: TextView
-
-    private lateinit var ivFirst: ImageView
-    private lateinit var ivSecond: ImageView
-    private lateinit var ivThird: ImageView
-    private lateinit var ivLast: ImageView
+    private lateinit var binding: ActivityDashboardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
-
-        homeRecyclerView = findViewById(R.id.rv_home_list)
-        progressBar = findViewById(R.id.progressBar)
-        bottomMenuRoot = findViewById(R.id.bottom_menu_root)
-
-        lnFirst = findViewById(R.id.ln_first)
-        lnSecond = findViewById(R.id.ln_second)
-        lnThird = findViewById(R.id.ln_third)
-        lnLast = findViewById(R.id.ln_last)
-
-        bottomCenterMenu = findViewById(R.id.bottom_center_menu)
-        firstMenu = findViewById(R.id.bottom_first_menu)
-        secondMenu = findViewById(R.id.bottom_second_menu)
-        thirdMenu = findViewById(R.id.bottom_third_menu)
-        lastMenu = findViewById(R.id.bottom_last_menu)
-
-        ivFirst = findViewById(R.id.iv_bottom_first)
-        ivSecond = findViewById(R.id.iv_bottom_second)
-        ivThird = findViewById(R.id.iv_bottom_third)
-        ivLast = findViewById(R.id.iv_bottom_last)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         homeMenuParentAdapter = MenuParentAdapter(arrayListOf(), this)
 
-        homeRecyclerView.apply {
+        binding.rvHomeList.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = homeMenuParentAdapter
         }
@@ -106,7 +64,7 @@ class DashboardActivity : BaseActivity() {
         getMobileMenu()
 
         viewModel.mobileMenu.observe(this) {
-            progressBar.visible(it is Resource.Loading)
+            binding.progressBar.visible(it is Resource.Loading)
             when (it) {
                 is Resource.Success -> {
                     setBottomMenu(it.value.data.bottomParentMenu)
@@ -134,24 +92,35 @@ class DashboardActivity : BaseActivity() {
         }
 
         if (bottomMenu.size == 5) {
-            bottomMenuRoot.visible(true)
+            binding.bottomMenuRoot.visible(true)
+            val bottomCenterMenu = binding.bottomCenterMenu
+
             bottomCenterMenu.setOnClickListener {
                 menuRouting(this, bottomMenu[2].featureId)
-                // findNavController().navigate(R.id.action_dashboardFragment_to_productFragment)
             }
-            lnFirst.setOnClickListener {
+            binding.lnFirst.setOnClickListener {
                 menuRouting(this, bottomMenu[0].featureId)
             }
 
-            lnSecond.setOnClickListener {
+            binding.lnSecond.setOnClickListener {
                 menuRouting(this, bottomMenu[1].featureId)
             }
-            lnThird.setOnClickListener {
+            binding.lnThird.setOnClickListener {
                 menuRouting(this, bottomMenu[3].featureId)
             }
-            lnLast.setOnClickListener {
+            binding.lnLast.setOnClickListener {
                 menuRouting(this, bottomMenu[4].featureId)
             }
+
+            val firstMenu = binding.bottomFirstMenu
+            val secondMenu = binding.bottomSecondMenu
+            val thirdMenu = binding.bottomThirdMenu
+            val lastMenu = binding.bottomLastMenu
+
+            val ivFirst = binding.ivBottomFirst
+            val ivSecond = binding.ivBottomSecond
+            val ivThird = binding.ivBottomThird
+            val ivLast = binding.ivBottomLast
 
             for ((count, bottom) in bottomMenu.withIndex()) {
                 when (count) {
@@ -203,20 +172,48 @@ class DashboardActivity : BaseActivity() {
 
     private fun getMenuIcon(featureId: String): Int {
         when (featureId) {
-            ORDER -> return R.drawable.ic_bt_order_unselect
-            DEPOSIT -> return R.drawable.ic_bt_deposit_unselect
-            TA_DA -> return R.drawable.ic_bt_ta_da_unselect
-            NOTICE -> return R.drawable.ic_bt_warning_unselect
-            DCR -> return R.drawable.ic_bt_visit_un_select
-            TRACKING -> return R.drawable.ic_bt_tracking_unselect
-            RETURN -> return R.drawable.ic_bt_return
-            OFFER -> return R.drawable.ic_bt_offers
-            PAYMENT_COLLECTION -> return R.drawable.ic_bt_payments
-            PRODUCT_LIST -> return R.drawable.ic_bt_product
-            CUSTOMER_LIST -> return R.drawable.ic_bt_customer
-            REVIEW_ORDER -> return R.drawable.ic_hm_review_order
-            REVIEW_REQUEST -> return R.drawable.ic_hm_review
-            REVIEW_MTP -> return R.drawable.ic_hm_review_mtp
+            ORDER -> {
+                return R.drawable.ic_bt_order_unselect
+            }
+            DEPOSIT -> {
+                return R.drawable.ic_bt_deposit_unselect
+            }
+            TA_DA -> {
+                return R.drawable.ic_bt_ta_da_unselect
+            }
+            NOTICE -> {
+                return R.drawable.ic_bt_warning_unselect
+            }
+            DCR -> {
+                return R.drawable.ic_bt_visit_un_select
+            }
+            TRACKING -> {
+                return R.drawable.ic_bt_tracking_unselect
+            }
+            RETURN -> {
+                return R.drawable.ic_bt_return
+            }
+            OFFER -> {
+                return R.drawable.ic_bt_offers
+            }
+            PAYMENT_COLLECTION -> {
+                return R.drawable.ic_bt_payments
+            }
+            PRODUCT_LIST -> {
+                return R.drawable.ic_bt_product
+            }
+            CUSTOMER_LIST -> {
+                return R.drawable.ic_bt_customer
+            }
+            REVIEW_ORDER -> {
+                return R.drawable.ic_hm_review_order
+            }
+            REVIEW_REQUEST -> {
+                return R.drawable.ic_hm_review
+            }
+            REVIEW_MTP -> {
+                return R.drawable.ic_hm_review_mtp
+            }
         }
         return R.drawable.ic_no_image_hm
     }
