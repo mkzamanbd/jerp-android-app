e2208089a98322cce1f3d012b7f281039934156e
Kamruzzaman <zaman7u@gmail.com>
2022-02-14 00:51:26 +0600

    Update ProfileViewModel.kt

--- app/src/main/java/me/kzaman/android/ui/viewModel/ProfileViewModel.kt
+++ app/src/main/java/me/kzaman/android/ui/viewModel/ProfileViewModel.kt
@@ -9,19 +9,24 @@ import me.kzaman.android.network.Resource
 import me.kzaman.android.repository.ProfileRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import me.kzaman.android.database.AppDatabase
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
     private val repository: ProfileRepository,
+    private val appDatabase: AppDatabase,
 ) : BaseViewModel(repository) {
 
     private val _profile: MutableLiveData<Resource<ProfileResponse>> = MutableLiveData()
-    val profile: LiveData<Resource<ProfileResponse>>
-        get() = _profile
+    val profile: LiveData<Resource<ProfileResponse>> = _profile
 
     fun getUserProfile() = viewModelScope.launch {
         _profile.value = Resource.Loading
         _profile.value = repository.getUserProfile()
+    }
+
+    fun clearAllTable() = viewModelScope.launch {
+        appDatabase.clearAllTables()
     }
 }
